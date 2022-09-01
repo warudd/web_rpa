b7a5f3b88538058f177ae4ba21d38b42f1fcd5d3
warudd <warudminsuwan@hotmail.com>
2022-09-01 14:52:57 +0700

    start project

---
+++ rpa.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+var {mssql_port} = process.env;
+var express = require('express');
+var app = express();
+app.use(express.json());
+var sql = require('mssql');
+const bodyParser = require('body-parser');
+const req = require('express/lib/request');
+const { request } = require('http');
+const { type } = require('express/lib/response');
+const { parse } = require('path');
+app.use(bodyParser.json());
+
+app.listen(3000, () => {
+    console.log('Successfully on port 3000');
+});
